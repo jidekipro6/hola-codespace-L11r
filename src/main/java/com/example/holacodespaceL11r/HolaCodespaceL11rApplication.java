d8048e9271abea6a30b117176cbe109835c70f9e
jidekipro6 <55932100+jidekipro6@users.noreply.github.com>
2023-06-26 17:47:59 +0000

    avanze

---
+++ src/main/java/com/example/holacodespaceL11r/HolaCodespaceL11rApplication.java
@@ -0,0 +1,13 @@
+package com.example.holacodespaceL11r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaCodespaceL11rApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaCodespaceL11rApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/holacodespaceL11r/HolaCodespaceL11rApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holacodespaceL11r;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaCodespaceL11rApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
